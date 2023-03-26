d9091b0354e8d09285c8b5b7b693bcdd99413cc3
antoniomelo <antoniopmeloferreira@gmail.com>
2023-03-26 14:56:13 +0100

    .net project

---
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,57 @@
+﻿using Mango.Services.ProductAPI.DbContexts.Models.Dto;
+using Mango.Services.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ProductAPI.Controllers
+{
+    [Route("api/products")]
+    public class ProductAPIController : ControllerBase
+    {
+        protected ResponseDto _response;
+        private IProductRepository _productRepository;
+
+        public ProductAPIController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+            this._response = new ResponseDto();
+        }
+
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            try
+            {
+                /*
+                 * IEnumerable is more efficient and faster when you only need to enumerate the data once.
+                 * The List is more efficient when you need to enumerate the data multiple times because it 
+                 * already has all of it in memory.
+                 */
+                IEnumerable<ProductDto> productsDtos = await _productRepository.GetProducts();
+                _response.Result= productsDtos;
+            }
+            catch (Exception ex) 
+            { 
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+        
+         [HttpGet]
+         [Route("{id}")]
+        public async Task<object> Get(int id)
+        {
+            try
+            {
+                ProductDto productDto = await _productRepository.GetProductById(id);
+                _response.Result = productDto;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+    }
+}
---
+++ Mango.Services.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using Mango.Services.ProductAPI.DbContexts.Models.Dto;
+
+namespace Mango.Services.ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        /*
+         * The Task class represents a single operation that does not return a value and 
+         * that usually executes asynchronously. 
+         *
+         * IEnumerable is the base interface for all non-generic collections that can be enumerated.
+         * IEnumerator provides the ability to iterate through the collection 
+         * by exposing a Current property and MoveNext and Reset methods.
+         */
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductById(int id);
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+        Task<bool> DeleteProduct(int id);
+    }
+}
