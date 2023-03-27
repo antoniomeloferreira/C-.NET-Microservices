ae03c686f0ad99afe9f3cddfc826ac9b87ac4a20
antoniomelo <antoniopmeloferreira@gmail.com>
2023-03-27 22:27:12 +0100

    refactor

--- Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -4,104 +4,103 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Mango.Services.ProductAPI.Controllers
 {
-    [Route("api/products")]
-    public class ProductAPIController : ControllerBase
-    {
-        protected ResponseDto _response;
-        private IProductRepository _productRepository;
+	[Route("api/products")]
+	public class ProductAPIController : ControllerBase
+	{
+		protected ResponseDto _response;
+		private IProductRepository _productRepository;
 
-        public ProductAPIController(IProductRepository productRepository)
-        {
-            _productRepository = productRepository;
-            this._response = new ResponseDto();
-        }
+		public ProductAPIController(IProductRepository productRepository)
+		{
+			_productRepository = productRepository;
+			this._response = new ResponseDto();
+		}
 
-        [HttpGet]
-        public async Task<object> Get()
-        {
-            try
-            {
-                /*
+		[HttpGet]
+		public async Task<object> Get()
+		{
+			try
+			{
+				/*
                  * IEnumerable is more efficient and faster when you only need to enumerate the data once.
                  * The List is more efficient when you need to enumerate the data multiple times because it 
                  * already has all of it in memory.
                  */
-                IEnumerable<ProductDto> productsDtos = await _productRepository.GetProducts();
-                _response.Result= productsDtos;
-            }
-            catch (Exception ex) 
-            { 
-                _response.IsSuccess = false;
-                _response.ErrorMessages = new List<string>() { ex.ToString() };
-            }
-            return _response;
-        }
-        
-         [HttpGet]
-         [Route("{id}")]
-        public async Task<object> Get(int id)
-        {
-            try
-            {
-                ProductDto productDto = await _productRepository.GetProductById(id);
-                _response.Result = productDto;
-            }
-            catch (Exception ex)
-            {
-                _response.IsSuccess = false;
-                _response.ErrorMessages = new List<string>() { ex.ToString() };
-            }
-            return _response;
-        }
+				IEnumerable<ProductDto> productsDtos = await _productRepository.GetProducts();
+				_response.Result = productsDtos;
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.ErrorMessages = new List<string>() { ex.ToString() };
+			}
+			return _response;
+		}
 
-        [HttpPost]
-        [Route("{id}")]
-        public async Task<object> Post([FromBody] ProductDto productDto)
-        {
-            try
-            {
-                ProductDto model = await _productRepository.CreateUpdateProduct(productDto);
-                _response.Result = model;
-            }
-            catch (Exception ex)
-            {
-                _response.IsSuccess = false;
-                _response.ErrorMessages = new List<string>() { ex.ToString() };
-            }
-            return _response;
-        }
+		[HttpGet]
+		[Route("{id}")]
+		public async Task<object> Get(int id)
+		{
+			try
+			{
+				ProductDto productDto = await _productRepository.GetProductById(id);
+				_response.Result = productDto;
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.ErrorMessages = new List<string>() { ex.ToString() };
+			}
+			return _response;
+		}
 
-        [HttpPut]
-        public async Task<object> Put([FromBody] ProductDto productDto)
-        {
-            try
-            {
-                ProductDto model = await _productRepository.CreateUpdateProduct(productDto);
-                _response.Result = model;
-            }
-            catch (Exception ex)
-            {
-                _response.IsSuccess = false;
-                _response.ErrorMessages = new List<string>() { ex.ToString() };
-            }
-            return _response;
-        }
+		[HttpPost]
+		public async Task<object> Post([FromBody] ProductDto productDto)
+		{
+			try
+			{
+				ProductDto model = await _productRepository.CreateUpdateProduct(productDto);
+				_response.Result = model;
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.ErrorMessages = new List<string>() { ex.ToString() };
+			}
+			return _response;
+		}
 
-        [HttpDelete]
-        [Route("{id}")]
-        public async Task<object> Delete(int id)
-        {
-            try
-            {
-                bool isSuccess = await _productRepository.DeleteProduct(id);
-                _response.Result = isSuccess;
-            }
-            catch (Exception ex)
-            {
-                _response.IsSuccess = false;
-                _response.ErrorMessages = new List<string>() { ex.ToString() };
-            }
-            return _response;
-        }
-    }
+		[HttpPut]
+		public async Task<object> Put([FromBody] ProductDto productDto)
+		{
+			try
+			{
+				ProductDto model = await _productRepository.CreateUpdateProduct(productDto);
+				_response.Result = model;
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.ErrorMessages = new List<string>() { ex.ToString() };
+			}
+			return _response;
+		}
+
+		[HttpDelete]
+		[Route("{id}")]
+		public async Task<object> Delete(int id)
+		{
+			try
+			{
+				bool isSuccess = await _productRepository.DeleteProduct(id);
+				_response.Result = isSuccess;
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.ErrorMessages = new List<string>() { ex.ToString() };
+			}
+			return _response;
+		}
+	}
 }
